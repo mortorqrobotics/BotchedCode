c769d1950346c84c62f3441358953c7b4b78be80
Hayley Chechel <96212232+hacysac@users.noreply.github.com>
2025-03-01 11:20:29 -0800

    pivot fixes

--- src/main/java/frc/BotchedCode/Subsystems/Pivot.java
+++ src/main/java/frc/BotchedCode/Subsystems/Pivot.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -28,15 +27,15 @@ public class Pivot extends SubsystemBase{
         // talonFXConfigs.Feedback.FeedbackRemoteSensorID = mCANcoder.getDeviceID();
         // talonFXConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
 
-        talonFXConfigs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        //talonFXConfigs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
         var slot0Configs = talonFXConfigs.Slot0;
         slot0Configs.kP = 10; // An error of 1 rps results in 0.11 V output
         slot0Configs.kI = 0; // no output for integrated error
         slot0Configs.kD = 0; // no output for error derivative
 
         var motionMagicConfigs = talonFXConfigs.MotionMagic;
-        motionMagicConfigs.MotionMagicCruiseVelocity = 2; // Target cruise velocity of 80 rps
-        motionMagicConfigs.MotionMagicAcceleration = 4; // Target acceleration of 160 rps/s (0.5 seconds)
+        motionMagicConfigs.MotionMagicCruiseVelocity = 100; // Target cruise velocity of 80 rps
+        motionMagicConfigs.MotionMagicAcceleration = 200; // Target acceleration of 160 rps/s (0.5 seconds)
         motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
 
         mpivot.getConfigurator().apply(talonFXConfigs);
@@ -95,14 +94,14 @@ public class Pivot extends SubsystemBase{
     }
 
     public void resetEncoder(){
-        mpivot.setPosition(1*getCANCoderValue()+0);
+        encoderZeroed = true;
+        mpivot.setPosition(100*getCANCoderValue()+0);
     }
 
     @Override
     public void periodic(){
         SmartDashboard.putNumber("Pivot Angle", getPosition());
         SmartDashboard.putNumber("Pivot Cancoder Angle", mCANcoder.getAbsolutePosition().getValueAsDouble());
-        SmartDashboard.putNumber("Pivot Current", Math.abs(mpivot.getStatorCurrent().getValueAsDouble()));
         if (encoderZeroed){
             mpivot.setControl(m_request.withPosition(setpoint));
         }
