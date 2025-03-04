e021e3d7ffd7b30751aa1e8c02d268b1f5e484e6
Hayley <hayley.chechel@gmail.com>
2025-03-04 11:09:32 -0800

    strafe to tag fix

--- src/main/java/frc/BotchedCode/Commands/StrafeToTag.java
+++ src/main/java/frc/BotchedCode/Commands/StrafeToTag.java
@@ -1,6 +1,7 @@
 package frc.BotchedCode.Commands;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.BotchedCode.Constants.RobotMap;
 import frc.BotchedCode.RobotContainer;
@@ -13,7 +14,9 @@ public class StrafeToTag extends Command {
     private PIDController yController;
     private double xSetpoint; 
     private double ySetpoint;
-    private double offset;
+    private double xOffset;
+    private double yOffset;
+    private double tagHeight;
     
 
     /**
@@ -22,10 +25,11 @@ public class StrafeToTag extends Command {
      * @param drivetrainSubsystem
      * @param limelight
      */
-    public StrafeToTag(CommandSwerveDrivetrain drivetrainSubsystem, double offset) {
+    public StrafeToTag(CommandSwerveDrivetrain drivetrainSubsystem, double xOffset, double yOffset) {
         this.drivetrainSubsystem = drivetrainSubsystem;
-        this.offset = offset;
-        double kp = 1, ki = 3, kd = 0, tolerance = 0.01;
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+        double kp = 1, ki = 3, kd = 0, tolerance = 0.02;
 
         xController = new PIDController(kp, ki, kd);
         // TODO tune PID and tolerance
@@ -46,33 +50,36 @@ public class StrafeToTag extends Command {
         yController.reset();
 
         var tagPose = RobotMap.ANDYMARK_FIELD2025.getTagPose((int) LimelightHelpers.getFiducialID("limelight")).get();
-        double angle = RobotContainer.drivetrain.getState().Pose.getRotation().getRadians();
-        xSetpoint = tagPose.getX() - offset*Math.cos(angle);
-        ySetpoint = tagPose.getY() - offset*Math.sin(angle);
+        tagHeight = tagPose.getZ();
+        //double angle = RobotContainer.drivetrain.getState().Pose.getRotation().getRadians();
+        //xSetpoint = tagPose.getX() - offset*Math.cos(angle);
+        //ySetpoint = tagPose.getY() - offset*Math.sin(angle);
+        xSetpoint = xOffset;
+        ySetpoint = yOffset;
     }
 
     @Override
     public void execute() {
-        //double floorDist = tagHeight/Math.tan(Units.degreesToRadians(LimelightHelpers.getTY(RobotMap.LIMELIGHT_NAME) + 30));
-        //double tx = LimelightHelpers.getTX(RobotMap.LIMELIGHT_NAME);
-        //System.out.println("FloorDist: " + floorDist + "   TX: " + tx);
-        //double xSpeed = xController.calculate(floorDist, xOffset);
-        //double ySpeed = yController.calculate(tx, 0);
-        var robotPos = RobotContainer.drivetrain.getState().Pose;
+        double ty = LimelightHelpers.getTY(RobotMap.LIMELIGHT_NAME) + LimelightHelpers.getCameraPose3d_RobotSpace(RobotMap.LIMELIGHT_NAME).getRotation().getY();
+        double tx = LimelightHelpers.getTX(RobotMap.LIMELIGHT_NAME);
+        double xDist = tagHeight/Math.tan(Units.degreesToRadians(ty));
+        double yDist = xDist*Math.tan(Units.degreesToRadians(tx));
+        double xSpeed = xController.calculate(xDist, xOffset);
+        double ySpeed = yController.calculate(yDist, yOffset);
+        //var robotPos = RobotContainer.drivetrain.getState().Pose;
 
-        double xSpeed = xController.calculate(robotPos.getX(), xSetpoint);
-        double ySpeed = yController.calculate(robotPos.getY(), ySetpoint);
+        // double xSpeed = xController.calculate(robotPos.getX(), xSetpoint);
+        // double ySpeed = yController.calculate(robotPos.getY(), ySetpoint);
 
-        System.out.println("XPose: " + robotPos.getX() + "   YPose: " + robotPos.getY());
-        System.out.println("FXPose: " + xSetpoint + "   FYPose: " + ySetpoint);
-        System.out.println("xSpeed: " + xSpeed + "   ySpeed: " + ySpeed);
+        // System.out.println("XPose: " + robotPos.getX() + "   YPose: " + robotPos.getY());
+        // System.out.println("FXPose: " + xSetpoint + "   FYPose: " + ySetpoint);
+        // System.out.println("xSpeed: " + xSpeed + "   ySpeed: " + ySpeed);
                 
         drivetrainSubsystem.setControl(
-            RobotContainer.drive.withVelocityX(xSpeed) // Drive forward with negative Y (forward)
-            .withVelocityY(ySpeed) // Drive left with negative X (left)
+            RobotContainer.forwardStraight.withVelocityX(xSpeed)
+            .withVelocityY(ySpeed)
             .withRotationalRate(0)
-            
-        ); // Drive counterclockwise with negative X (left)
+        );
     }
 
     @Override
--- src/main/java/frc/BotchedCode/RobotContainer.java
+++ src/main/java/frc/BotchedCode/RobotContainer.java
@@ -63,7 +63,7 @@ public class RobotContainer {
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-    private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric()
+    public final static SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric()
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
